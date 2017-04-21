c5070d4470c21fcb13da538b97435d039832aee4
Joe Flowers <jflowers45@gmail.com>
2017-04-21 14:15:32 -0400

    code cleanup, and also faking a window resize to cure issue with line not drawing

--- functions.php
+++ functions.php
@@ -104,14 +104,17 @@ function ttpmap_func( $atts ) {
 		// --><button type="button" title="CUBA" class=" btn-default cuba" value="cuba"><span class="bbg__map__button-text">Partially Free</span></button><!--
 		// --></div>
 		?>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<h2>Threats to Press</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non lacus velit. Proin porta ultricies ex non vulputate. Aenean maximus convallis varius. Invidual threats are mapped below with the <i class="fa fa-map-pin" aria-hidden="true"></i> icon and you may <a style="text-decoration: underline;" href="https://www.bbg.gov/2016-threats-archive/" target="_blank">view a full list</a> on the bbg.gov site.</p>
 		<div class="bbg__map-area__container " style="postion: relative;">
 			<div id="chartdiv"></div>
 			<div align="center" >
 						<div align="center" >
 							<div class="legendBox free"></div> Free 
-							<div class="legendBox not-free"></div> Partially Free 
-							<div class="legendBox partially-free"></div> Not Free 
+							<div class="legendBox partially-free"></div> Partially Free 
+							<div class="legendBox not-free"></div> Not Free 
+							&nbsp;&nbsp;<span style="white-space: nowrap;"><i class="fa fa-map-pin" aria-hidden="true"></i> Incident</span>
 						</div>
 					</div>
 		</div>
@@ -120,229 +123,8 @@ function ttpmap_func( $atts ) {
 	echo "<script type='text/javascript'>\n";
 	echo "freeNotFree = $freeNotFreeStr";
 	echo "</script>";
-
-
-	$str = ob_get_clean();
-
-    return $str;
-}
-add_shortcode( 'ttpmap', 'ttpmap_func' ); 
-vc_map( array(
-   "name" => __("Threats to Press Map"),
-   "base" => "ttpmap",
-   "category" => __('Content'),
-   "params" => array(
-      array(
-         "type" => "textfield",
-         "holder" => "div",
-         "class" => "",
-         "heading" => __("Text"),
-         "param_name" => "foo",
-         "value" => __("Default params value"),
-         "description" => __("Description for foo param.")
-      )
-   )
-) );
-
-
-function videointro_func( $atts ) {
-    $bgSource = "https://bbgredesign.voanews.com/wp-content/themes/bbgRedesign/tools/1280_1.jpg"; 
-    $popupSrc = "https://www.youtube.com/embed/0Uy-IDrIe7w?enablejsapi=1&origin=https://www.bbgannual.com";
-    //https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=https://bbgredesign.voanews.com
-    //https://www.youtube.com/watch?v=0Uy-IDrIe7w
-    //https://player.vimeo.com/video/171645924?api=1&player_id=videoplayer&color=000000
-
-    //$bgVidSource = "https://bbgredesign.voanews.com/wp-content/themes/bbgRedesign/tools/ar.mov";
-    $bgVidSource = "https://bbgredesign.voanews.com/wp-content/themes/bbgRedesign/tools/ar_reducedSize.mp4";
-
-   ob_start();
-
 ?>
-		
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
-	<script type="text/javascript">
-		function toggleVideo(state) { 
-			// if state == 'hide', hide. Else: show video
-			var div = document.getElementById("popup-video");
-			div.style.display = state == 'hide' ? 'none' : 'block';
-			var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
-			func = state == 'hide' ? 'pauseVideo' : 'playVideo';
-			iframe.postMessage('{"event":"command","func":"' + func + '","args":""}','*');
-		}
-	</script>
-
-
 	<style>
-		#introvideorow .wpb_column {
-			padding-left: 0px !important;
-			padding-right: 0px !important;
-		}
-		#introvideorow {
-			margin-bottom:0px;
-		}
-		body {
-			margin:0px;
-		}
-		.header {
-			height: 780px;
-			position: relative;
-			text-align: center;
-		} 
-		@media screen and (max-width: 767px) {
-		    .header  {
-		        height: 572px;
-		    }
-		}
-		.background-video-outer {
-			content: ' ';
-			display: block;
-			position: absolute;
-			width: 100%;
-			height: 100%;
-			left: 0px;
-			top: 0px;
-			/*background: #FF0000;*/
-			z-index: 5;
-		}
-		.background-video-inner {
-			width: 100%;
-		    height: 100%;
-		    position: relative;
-		}
-		.background-video-source  {
-			z-index: 2;
-			width: 100%;
-			height: 100%;
-			left: 0px;
-			top: 0px;
-			overflow: hidden;
-			position: absolute
-		}
-		.background-video-overlay {
-			background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAQAAAAziH6sAAAADklEQVQYV2NgmMmgvQMAA3gBfXW5VPgAAAAASUVORK5CYII=);
-			width: 100%;
-			height: 100%;
-			left: 0px;
-			top: 0px;
-			overflow: hidden;
-			position: absolute;
-			z-index: 3
-		}
-		.background-video-source video {
-		    height: 100%;
-		    width: auto
-		}
-		@media screen and (min-width: 1380px) {
-		    .background-video-source video {
-		        height: auto;
-		        width: 100%
-		    } 
-		}
-
-		.block {
-			position: relative;
-			width:100%;
-		}
-
-		.block .content {
-			margin: 0px auto;
-			max-width: 952px;
-			padding-right: 80px;
-			padding-left: 80px;
-			*zoom: 1
-		}
-		.block .content h2 {
-			color:#FFF;
-		}
-		.block .content:after {
-			content: "";
-			display: table;
-			clear: both
-		}
-		.header .block {
-			padding-top: 120px;
-			height: 100%;
-			z-index: 15;
-			color:#FFF !important;
-		}		
-		.header button.play {
-			width: 40px;
-			height: 40px;
-			margin: 0px auto;
-			text-align: center;
-			text-shadow: 1px 1px 5px rgba(0, 0, 0, 0.65);
-			cursor:pointer;
-		}
-		#popup-video {
-		    height: 100%;
-		    width: 100%;
-		    position: absolute;
-		    top: 0px;
-		    left: 0px;
-		    z-index: 25;
-		    background: rgba(45, 45, 45, 0.9);
-		    filter: progid: DXImageTransform.Microsoft.Alpha(Opacity=0);
-		    display: none;
-		}
-		.header .popup-video a.close.close-video {
-		    width: 86px;
-		    height: 86px;
-		    margin: 6px auto 0px auto;
-		    color:#FFF;
-		}
-		.popup-video-holder {
-			position: relative;
-			padding-bottom: 56.25%;
-			padding-top: 30px; height: 0; overflow: hidden;
-		}
-		.popup-video-holder iframe,
-		.popup-video-holder object,
-		.popup-video-holder embed {
-			position: absolute;
-			top: 0;
-			left: 0;
-			width: 100%;
-			height: 100%;
-		}
-
-	</style>
-
-</head>
-<body>
-
-	 <div class="header">
-	 	<div class="background-video-outer">
-	 		<div class="background-video-inner">
-	 			<div class="background-video-overlay"></div>
-	 			<div class="background-video-source">
-	 				<video width=1280 height=720 autoplay loop muted poster="<?php echo $bgSource; ?>" id=main_video>
-						<source src="<?php echo $bgVidSource; ?>" type="video/mp4">
-						<img src="<?php echo $bgSource; ?>" alt="" class=video-fallback /> 
-					</video>
-				</div>
-			</div>
-		</div>
-		<div class=block>
-			<div class=content>
-				<h2>Serving Freedom<br>and Democracy</h2>
-				<i id="btnPlay" class="fa fa-play fa-2x" style="cursor:pointer"></i>
-				<!--<p>Broadcasting Board of Governors<BR>2016 Annual Report</p> -->
-				<!-- <div class=advance><button></button></div> -->
-			</div>
-		</div>
-		<div class=popup-video id="popup-video">
-			<div class=block>
-				<div class=content>
-					<div class=popup-video-holder> <iframe id=videoplayer src="<?php echo $popupSrc; ?>" width=793 height=470 frameborder=0 web></iframe> </div>
-					<a href="#" class="close close-video"><span>Close</span></a> 
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<style>
-
 		div[id^='circleTooltip_'] {
 			padding: 20px;
 			border: 1px solid #EBEBEB;
@@ -364,6 +146,9 @@ function videointro_func( $atts ) {
 		#circleTooltip_rferl {
 			background: #887639;
 		}
+		.vc_chart-legend li {
+			list-style-type: none !important;
+		}
 	</style>
 
 	<script type="text/javascript">
@@ -379,15 +164,7 @@ function videointro_func( $atts ) {
 		}
 
 		jQuery( document ).ready(function() {
-			jQuery('#btnPlay').click(function(e) {
-				toggleVideo('');
-			});
-			jQuery('.close').click(function(e) {
-				toggleVideo('hide');
-			});
-
 			jQuery("path[class$='_circle'],g[class$='_circle']").css('cursor','pointer'); 			
-
 			jQuery("path[class$='_circle'],g[class$='_circle']").hover( 
 				function(e) {
 					var entity = jQuery(this).attr('class').split("_").shift();
@@ -397,38 +174,55 @@ function videointro_func( $atts ) {
 					toggleCircleDiv('','out');
 				}
 			);
-
 			jQuery('#circleTooltip').hide() //.addClass('hb-testimonial');
+
+			//there is an issue with the Visual Composer line chart that happens on the hoempage
+			function fakeWindowResize(){
+				jQuery(window).resize();
+			}
+			setTimeout(fakeWindowResize, 2000); 
+
+			// jQuery('.popmake-933') 
+			// 	.on('pumBeforeOpen', function () {
+			// 		console.log("let us add the autoplay"); 
+			// 		var $iframe = jQuery('iframe', jQuery(this)),
+			// 			src = $iframe.prop('src');
+			// 		$iframe.prop('src', '').prop('src', src + '?autoplay=1');
+			// 	});
+			// 	jQuery('#pum-123')
+			// 	.on('pumBeforeClose', function () {
+			// 		var $iframe = jQuery('iframe', jQuery(this)),
+			// 			src = $iframe.prop('src');
+			// 		$iframe.prop('src', '').prop('src', src.replace('?autoplay=1', ''));
+			// 	});
 		});
 
 	</script>
-
-
+ 
 <?php 
 
-	$str = ob_get_clean();
+		$str = ob_get_clean();
+	    return $str;
+	}
+	add_shortcode( 'ttpmap', 'ttpmap_func' ); 
+	vc_map( array(
+	   "name" => __("Threats to Press Map"),
+	   "base" => "ttpmap",
+	   "category" => __('Content'),
+	   "params" => array(
+	      array(
+	         "type" => "textfield",
+	         "holder" => "div",
+	         "class" => "",
+	         "heading" => __("Text"),
+	         "param_name" => "foo",
+	         "value" => __("Default params value"),
+	         "description" => __("Description for foo param.")
+	      )
+	   )
+	) );
 
-    return $str;
-}
-add_shortcode( 'videointro', 'videointro_func' ); 
-vc_map( array(
-   "name" => __("Intro Video"),
-   "base" => "videointro",
-   "category" => __('Content'),
-   "params" => array(
-      array(
-         "type" => "textfield",
-         "holder" => "div",
-         "class" => "",
-         "heading" => __("Text"),
-         "param_name" => "foo",
-         "value" => __("Default params value"),
-         "description" => __("Description for foo param.")
-      )
-   )
-) );
-
-?>
+/*
 
 <style>
 	section#ceoBackgroundVideo {
@@ -439,3 +233,6 @@ vc_map( array(
 		background-size: cover;
 	}
 </style>
+*/
+
+?>
