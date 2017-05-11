291f2786662182b4fc71076b1241b7529e54c63a
Joe Flowers <jflowers45@gmail.com>
2017-05-11 12:16:17 -0400

    maps / hotspot

--- functions_maps.php
+++ functions_maps.php
@@ -2,7 +2,7 @@
 
 function ttpmap_func( $atts ) {
 
-	$freeNotFreeObj = array_map('str_getcsv', file(get_stylesheet_directory_uri() . '/freeNotFree.csv'));
+	$freeNotFreeObj = array_map('str_getcsv', file(get_stylesheet_directory_uri() . '/data/freeNotFree.csv'));
 	if (count($freeNotFreeObj)) {
 
 		//remove the first row from the array because it's headers, not data
@@ -12,77 +12,73 @@ function ttpmap_func( $atts ) {
 	
     ob_start();
 ?>
-		<!-- Styles -->
-		<style>
-			/* start expanding the print styles */
+	<style>
+		#chartdiv {
+			border: 1px solid #CCC;
+			font-size: 11px;
+			height: 200px;
+			width: 100%;
+		}
+		@media screen and (min-width: 600px) {
 			#chartdiv {
-				border: 1px solid #CCC;
-				font-size: 11px;
-				height: 200px;
-				width: 100%;
+				height: 400px;
 			}
-			@media screen and (min-width: 600px) {
-				#chartdiv {
-					height: 400px;
-				}
+		}
+		@media screen and (min-width: 900px) {
+			#chartdiv {
+				height: 500px;
 			}
-			@media screen and (min-width: 900px) {
-				#chartdiv {
-					height: 500px;
-				}
-			}
-			.amcharts-legend-div {
-				position: fixed !important;
-				top: 520px !important;
-				padding: 10px;
-			}
+		}
+		.amcharts-legend-div {
+			position: fixed !important;
+			top: 520px !important;
+			padding: 10px;
+		}
 
-			.amcharts-chart-div > a {
-				display: none !important;
-			}
+		.amcharts-chart-div > a {
+			display: none !important;
+		}
 
-			#loading {
-				display: none;
-				z-index: 9997; 
-				position: absolute;
-				bottom: 5%;
-				left: 5%;
-				width: 50px;
-				height: 50px;
-			}
-			.legendBox {
-				width: 15px;
-				height: 15px;
-				display:inline-block;
-				background: #000000;
-			}
-			
-		</style>
-
-		<!-- Resources -->
-		<script src="https://www.amcharts.com/lib/3/ammap.js"></script>
-		<script src="https://www.amcharts.com/lib/3/maps/js/worldLow.js"></script>
+		#loading {
+			display: none;
+			z-index: 9997; 
+			position: absolute;
+			bottom: 5%;
+			left: 5%;
+			width: 50px;
+			height: 50px;
+		}
+		.legendBox {
+			width: 15px;
+			height: 15px;
+			display:inline-block;
+			background: #000000;
+		}
 		
-		<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/threats.js'></script>
-		<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/map-pressFreedomAndThreats.js'></script>
+	</style>
 
-		<!-- HTML --> 
-		<?php 
-		?>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<h2>Threats to Press</h2>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non lacus velit. Proin porta ultricies ex non vulputate. Aenean maximus convallis varius. Invidual threats are mapped below with the <i class="fa fa-map-pin" aria-hidden="true"></i> icon and you may <a style="text-decoration: underline;" href="https://www.bbg.gov/2016-threats-archive/" target="_blank">view a full list</a> on the bbg.gov site.</p>
-		<div class="bbg__map-area__container " style="postion: relative;">
-			<div id="chartdiv"></div>
+	<!-- Resources -->
+	<script src='<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/ammap.js'></script>
+	<script src='<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/mapdata-worldLow.js'></script>
+	
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/threats.js'></script>
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/map-annualreport.js'></script>
+
+	<!-- HTML --> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<h2>Threats to Press</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non lacus velit. Proin porta ultricies ex non vulputate. Aenean maximus convallis varius. Invidual threats are mapped below with the <i class="fa fa-map-pin" aria-hidden="true"></i> icon and you may <a style="text-decoration: underline;" href="https://www.bbg.gov/2016-threats-archive/" target="_blank">view a full list</a> on the bbg.gov site.</p>
+	<div class="bbg__map-area__container " style="postion: relative;">
+		<div id="chartdiv"></div>
+		<div align="center" >
 			<div align="center" >
-						<div align="center" >
-							<div class="legendBox free"></div> Free 
-							<div class="legendBox partially-free"></div> Partially Free 
-							<div class="legendBox not-free"></div> Not Free 
-							&nbsp;&nbsp;<span style="white-space: nowrap;"><i class="fa fa-map-pin" aria-hidden="true"></i> Incident</span>
-						</div>
-					</div>
+				<div class="legendBox free"></div> Free 
+				<div class="legendBox partially-free"></div> Partially Free 
+				<div class="legendBox not-free"></div> Not Free 
+				&nbsp;&nbsp;<span style="white-space: nowrap;"><i class="fa fa-map-pin" aria-hidden="true"></i> Incident</span>
+			</div>
 		</div>
+	</div>
 <?php 
 
 	echo "<script type='text/javascript'>\n";
@@ -91,7 +87,6 @@ function ttpmap_func( $atts ) {
 ?>
  
 <?php 
-
 		$str = ob_get_clean();
 	    return $str;
 	}
@@ -113,3 +108,191 @@ function ttpmap_func( $atts ) {
 	   )
 	));
 ?>
+<?php
+
+function hotspotmap_func( $atts ) {
+
+    ob_start();
+?>
+	
+	<script type="text/javascript">
+		bbgConfig={};
+		bbgConfig.MAPBOX_API_KEY = 'pk.eyJ1IjoiYmJnd2ViZGV2IiwiYSI6ImNpcDVvY3VqYjAwbmx1d2tyOXlxdXhxcHkifQ.cD-q14aQKbS6gjG2WO-4nw';
+		bbgConfig.template_directory_uri = 'https://www.bbg.gov/wp-content/themes/bbgRedesign/';
+
+		entities={
+		    "ocb": {
+		        "description": "<p>OCB oversees Radio and Television Mart\u00ed at its headquarters in Miami, Florida. Combined with the online platform, martinoticias.com, the Mart\u00eds are a one-of-a-kind service that brings unbiased, objective information to all Cubans.</p>\n",
+		        "fullName": "Office of Cuba Broadcasting",
+		        "url": "http://www.martinoticias.com/"
+		    },
+		    "rfa": {
+		        "description": "<p>RFA journalists provide uncensored, fact-based news to citizens of these countries, among the world\u2019s worst media environments. </p>\n",
+		        "fullName": "Radio Free Asia",
+		        "url": "http://www.rfa.org/"
+		    },
+		    "mbn": {
+		        "description": "<p>MBN is the non-profit news organization that operates Alhurra Television, Radio Sawa and MBN Digital reaching audiences in 22 countries across the Middle East and North Africa. </p>\n",
+		        "fullName": "Middle East Broadcasting Networks",
+		        "url": "http://www.alhurra.com/"
+		    },
+		    "rferl": {
+		        "description": "<p>RFE/RL reaches 26.9 million people in  26  languages and in 23 countries, including Afghanistan, Iran, Pakistan, Russia, and Ukraine.</p>\n",
+		        "fullName": "Radio Free Europe / Radio Liberty",
+		        "url": "http://www.rferl.org/"
+		    },
+		    "voa": {
+		        "description": "<p>Voice of America provides trusted and objective news and information in 45 languages to a measured weekly audience of more than 236.6 million people around the world.</p>\n",
+		        "fullName": "Voice of America",
+		        "url": "http://www.voanews.com"
+		    },
+		    "bbg": {
+		        "description": "The mission of the Broadcasting Board of Governors is to inform, engage, and connect people around the world in support of freedom and democracy.",
+		        "fullName": "Broadcasting Board of Governors",
+		        "url": "https://www.bbg.gov"
+		    }
+		};
+	</script>
+
+	<style>
+		.legendBox {
+			width: 15px;
+			height: 15px;
+			display:inline-table;
+			background: #000000;
+		}
+		/* start expanding the print styles */
+		#chartdiv {
+		  border: 1px solid #CCC;
+		  font-size: 11px;
+		  height: 200px;
+		  width: 100%;
+		}
+		@media screen and (min-width: 600px) {
+		  #chartdiv {
+		    height: 400px;
+		  }
+		}
+		@media screen and (min-width: 900px) {
+		  #chartdiv {
+		    height: 500px;
+		  }
+		}
+
+		.amcharts-legend-div {
+		  position: fixed !important;
+		  top: 520px !important;
+		  padding: 10px;
+		}
+
+		.amcharts-chart-div > a {
+		  display: none !important;
+		}
+		.u--hide-medium-large {
+		  display: inherit;
+		}
+		@media screen and (min-width: 900px) {
+		  .u--hide-medium-large {
+		    display: none;
+		  }
+		}
+
+		.u--show-medium-large {
+		  display: none;
+		}
+		@media screen and (min-width: 900px) {
+		  .u--show-medium-large {
+		    display: inherit;
+		  }
+		}
+		/*MOVED FROM BELOW TO UPDATE FONTS*/
+
+		button,
+		[type="button"] {
+		  margin-top: 0.25em;
+		  margin-right: 0.25em;
+		  margin-bottom: 0.25em;
+		  appearance: none;
+		  background-color: #0071bc;
+		  border: 0;
+		  border-radius: 0.15rem;
+		  color: #ffffff;
+		  cursor: pointer;
+		  display: inline-block;
+		  font-family: "Lato", "Helvetica", "Arial", sans-serif;
+		  font-size: 1rem;
+		  font-weight: 700;
+		  line-height: 1;
+		  outline: none;
+		  padding: .5rem 1rem;
+		  text-align: center;
+		  text-decoration: none;
+		  width: 100%;
+		  -webkit-font-smoothing: antialiased;
+		}
+		@media screen and (min-width: 481px) {
+		  button,
+		  [type="button"] {
+		    width: auto;
+		  }
+		}
+		.entity-buttons button { 
+			border-top:3px;
+			border-left:3px;
+			border-right:3px;
+			border-bottom:0px;
+			border-style: solid;
+			margin-right: 0em;
+		 	margin-bottom: 0em;
+		 	border-radius: 0em;
+		}
+	</style>
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/ammap.js'></script>
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/mapdata-worldLow.js'></script>
+	<script type='text/javascript' src='<?php echo get_stylesheet_directory_uri(); ?>/js/map-hotspot.js'></script>
+
+	
+	<div class="btn-group entity-buttons u--show-medium-large" role="group" aria-label="..." style="clear: none;">
+		<button type="button" title="ALL" class=" btn-default all" value="all"><span class="bbg__map__button-text">ALL</span></button><!--
+		--><button type="button" title="CHINA" class=" btn-default china" value="china"><span class="bbg__map__button-text">CHINA</span></button><!--
+		--><button type="button" title="CUBA" class=" btn-default cuba" value="cuba"><span class="bbg__map__button-text">CUBA</span></button><!--
+		--><button type="button" title="IRAN" class=" btn-default iran" value="iran"><span class="bbg__map__button-text">IRAN</span></button><!--
+		--><button type="button" title="RUSSIA" class=" btn-default russia" value="russia"><span class="bbg__map__button-text">RUSSIA</span></button><!--
+		--><button type="button" title="COUNTERING VIOLENT EXTREMISM" class=" btn-default cve" value="cve"><span class="bbg__map__button-text">COUNTERING VIOLENT EXTREMISM</span></button><!--
+		--></div>
+	<div align="center" id="mapFilters" class="u--hide-medium-large">
+		<BR>
+		<select id="hotSpotPicker">
+			<option value="all">All</option>
+			<option value="china">China</option>
+			<option value="cve">CVE</option>
+			<option value="cuba">Cuba</option>
+			<option value="iran">Iran</option>
+			<option value="russia">Russia</option>
+		</select>
+	</div>
+
+
+		<div id="chartdiv"></div>
+	<!-- <div style="margin-top:1rem;" class="u--show-medium-large"><em>Hover over a country to highlight other countries in its primary hot spot.  Use the buttons at the top to view hot spots one at a time.</em></div><BR> -->
+
+	<div align="left" class="u--hide-medium-large">
+		<div align="center" >
+			<div class="legendBox china"></div> China 
+			<div class="legendBox cuba"></div> Cuba 
+			<div class="legendBox iran"></div> Iran 
+			<div class="legendBox russia"></div> Russia<BR>
+			<div class="legendBox cve"></div> Countering Violent Extremism
+		</div>
+	</div>
+
+
+
+
+<?php 
+		$str = ob_get_clean();
+	    return $str;
+	}
+	add_shortcode( 'hotspotmap', 'hotspotmap_func' ); 
+?>
+
---
+++ js/map-hotspot.js
@@ -0,0 +1,408 @@
+var COLOR_HOVER = "#CC0000"; //the color that highlights a hot spot when you roll over a country
+var COLOR_ACTIVE = "#FFFFFF"; //background color when a button in the hot spot bar is active
+
+var hideUnselectedCountries = true;
+
+var color_secondary_darkest = "#981b1e";
+var color_primary = "#0071bc";
+var color_green = "#2e8540";
+var color_orange = "orange";
+var color_ocb = "#653792";
+var color_mbn = "#BB3530";
+
+//primary colors for our various hot spots
+var lisaFrankColors = {
+	iran: color_green,
+	russia: color_orange,
+	china: color_mbn,
+	cuba: color_ocb,
+	cve: color_primary,
+	all: color_secondary_darkest
+}
+var pastelColors = {
+	iran: "#83c2ba",
+	russia: "#ebdb8b",
+	china: "#a791b4",
+	cuba: "#85c5e3",
+	cve: "#C9B7AD",
+	all: "#999999"
+}
+var normalColors = {
+	iran: "#CC99B3",
+	russia: "#ebdb8b",
+	china: "#2B9ED4",
+	cuba: "#71B771",
+	cve: "#E67300",
+	all: "#999999"
+}
+
+colors = normalColors;
+
+//define each sphere an dthe countires it is comprised of and influences
+var spheres = {
+	iran: {
+		comprisedOf: ['Iran'],
+		influences: ['Turkmenistan', 'Afghanistan', 'Pakistan', 'Yemen', 'Iraq', 'Azerbaijan'],
+		color: colors['iran'],
+		label: "Iran"
+	},
+	russia: {
+		comprisedOf: ['Russia'],
+		influences: ['Ukraine','Estonia','Latvia','Lithuania','Belarus','Moldova','Syria','Kazakhstan','Uzbekistan','Turkmenistan','Tajikistan','Kyrgyzstan','Azerbaijan','Armenia','Georgia','Israel'],
+		color: colors['russia'],
+		label: "Russia"
+	},
+	cuba: {
+		comprisedOf: ['Cuba'],
+		influences: ['Venezuela','Colombia'],
+		color: colors['cuba'],
+		label: "Cuba"
+	},
+	cve: {
+		comprisedOf: ['Russia'],
+		influences: [
+			'Afghanistan','Armenia','Iran','Iraq','Kazakhstan','Kyrgyzstan','Pakistan','Tajikistan','Turkmenistan','Uzbekistan','Belarus','Georgia','Serbia',
+			'Bangladesh','Philippines','Thailand','Malaysia','India','Indonesia',
+			'Israel','Jordan','Kuwait','Lebanon','Saudi Arabia','Syria','Turkey','Yemen','Egypt','Morocco','Libya','Algeria',
+			'Mali', 'Mauritania', 'Chad', 'Western Sahara', 'Sudan', 'Eritrea', 'Ethiopia', 'Somalia', 'Cameroon', 'Benin', 'Niger', 'Burkina Faso', 'Nigeria', 'Kenya', 'Tanzania', 'Uganda'
+		],
+		color: colors['cve'],
+		label: "Countering Violent Extremism"
+	},
+	china: {
+		comprisedOf: ['China'],
+		influences: [
+			'Bangladesh','Bhutan', 'Nepal', 'Thailand', 'Myanmar', 'Vietnam', 'Lao People\'s Democratic Republic', 'North Korea', 'South Korea', 'Cambodia', 'Malaysia'  ],
+		color: colors['china'],
+		label: "China"
+	},
+};
+
+var fullCountryList = AmCharts.maps.worldLow.svg.g.path;
+var activeSphere = "all";
+
+cMap = {}; //this allows us to look a country's ID up from its title, so that the data can originally be entered as country titles
+cMapByID = {};
+for (var i = 0; i < fullCountryList.length; i++) {
+	c = fullCountryList[i];
+	c.spheres = [];
+	cMap[c.title] = c.id;
+	cMapByID[c.id] = c;
+}
+
+sMap = {};  //this maps countryIDs to spheres so that when you click a country, it takes you to a sphere.
+sAreas = {};
+fullSphereCountryList = [];
+
+for (var key in spheres) {
+	if (spheres.hasOwnProperty(key)) {
+		s = spheres[key];
+		sAreas[key] = [];
+		var sphereCountries = s.comprisedOf.concat(s.influences);
+		for (var i=0; i < sphereCountries.length; i++) {
+			var countryName = sphereCountries[i];
+			if (cMap.hasOwnProperty(countryName)) {
+				var countryID = cMap[countryName];
+				fullSphereCountryList.push(countryID);
+				sAreas[key].push(countryID);
+				if (! (sMap.hasOwnProperty(countryID))) {
+					sMap[countryID] = key;
+				}
+				cMapByID[countryID].spheres.push(key);
+			} else {
+				console.log('nothing found for ' + sphereCountries[i]);
+			}
+		}
+	}
+}
+
+function getAreas(aSphere) {
+	var areas = [];
+	for (var key in spheres) {
+		if (spheres.hasOwnProperty(key)) {
+			s = spheres[key];
+			if (aSphere == key || aSphere == "all") {
+				var sphereCountries = s.comprisedOf.concat(s.influences);
+				for (var i=0; i < sphereCountries.length; i++) {
+					var countryName = sphereCountries[i];
+					if (cMap.hasOwnProperty(countryName)) {
+						var countryID = cMap[countryName];
+						//if you're only showing a particular sphere, color it for the sphere
+						//if you're showing all spheres, color it by the 'primary' sphere
+						var newColor = s.color;
+						if (aSphere == "all") {
+							var c = cMapByID[countryID];
+							newColor = colors[c.spheres[0]];
+						}
+						var isComprised = (i < s.comprisedOf.length);
+						var alpha = 1;
+						if ( (aSphere != "all" && aSphere != "cve") && !isComprised) {
+							alpha = 0.7;
+							///console.log('comprised!!!!');
+						}
+						var a = {
+							id: countryID,
+							title: countryName,
+							color: newColor,
+							alpha: alpha
+						}
+						areas.push(a);
+					}
+				}
+			}
+		}
+	}
+
+	return areas;
+}
+
+function setActiveSphere(s) {
+	activeSphere=s;
+	var newAreas = getAreas(s);
+	map.dataProvider.areas = newAreas;
+	map.validateData();
+}
+
+function initMobileLegend() {
+	//assign colors to the mobile legend.  Only happens once, on load.
+	jQuery('.china').css('background-color', colors['china']);
+	jQuery('.cuba').css('background-color', colors['cuba']);
+	jQuery('.iran').css('background-color', colors['iran']);
+	jQuery('.russia').css('background-color', colors['russia']);
+	jQuery('.cve').css('background-color', colors['cve']);
+}
+
+function resetButton(btnName) {
+	var defaultButtonBG = colors[btnName];
+	var defaultButtonTextColor = "#FFF";
+	var btn = jQuery('.' + btnName);
+	if (btnName == activeSphere) {
+		btn.css('color',defaultButtonBG);
+		btn.css('background-color',COLOR_ACTIVE);
+	} else {
+		btn.css('color',defaultButtonTextColor);
+		btn.css('background-color',defaultButtonBG);
+		btn.css('border-color',defaultButtonBG);
+	}
+}
+
+function resetButtons(btnLeaveAlone) {
+	//reset all buttons to their original color
+	jQuery('.entity-buttons button').each(function (index, value) {
+		var val = jQuery(this).val();
+		resetButton(val);
+	});
+}
+
+(function ($,bbgConfig, entities) {
+
+	jQuery(document).ready(function() {
+
+		map = AmCharts.makeChart( "chartdiv", {
+			theme: "light",
+			projection:"eckert3",
+			type: "map",
+			dataProvider: {
+				map: "worldLow",
+				areas:getAreas("all")
+			},
+			areasSettings: {
+				autoZoom: false,
+				alpha: 1,
+				unlistedAreasAlpha: 0.55,
+				selectable: true,
+				selectedColor: undefined,
+				outlineThickness: 0.1
+			},
+			zoomControl:  {
+				zoomControlEnabled: false,
+				panControlEnabled: false,
+				homeButtonEnabled: false
+			},
+			dragMap:false
+		});
+
+		map.addListener("clickMapObject", function (event) {
+			var sphere= false;
+			console.log('activeSphere is ' + activeSphere);
+			//if we're in the 'all' view, take the most relevant sphere
+			if (activeSphere == "all") {
+				sphere = sMap[event.mapObject.id];
+			} else {
+			//else if our view is restricted to a sphere, use that
+				sphere = activeSphere;
+			}
+			window.location = bbgConfig.template_directory_uri + "../../../hot-spots/" + sphere;
+		});
+
+		map.balloonLabelFunction = function (area, map) {
+			var txt = "";
+			if (activeSphere != "all") {
+				txt= spheres[activeSphere].label + " Hot Spot";
+			} else {
+				var sphere = spheres[sMap[area.id]];
+		    	if (sphere) {
+		    		txt = sphere.label + " Hot Spot";
+		    	}
+			}
+	    	return txt;
+	    };
+
+		map.addListener("rollOutMapObject", function (event) {
+			jQuery('div.dynamicHotSpotText').html(''); 
+			var countryID = event.mapObject.id;
+			var primarySphere = sMap[countryID];
+			if (primarySphere) {
+				if (activeSphere != "all") {
+					primarySphere = activeSphere;
+				}
+				var c = cMapByID[countryID];
+				event.mapObject.color = colors[c.spheres[0]];
+				event.mapObject.validate();
+				var s = spheres[primarySphere];
+				var sphereCountries = s.comprisedOf.concat(s.influences);
+				var usedIDs = {};
+				for (var i=0; i < sphereCountries.length; i++) {
+					var countryName = sphereCountries[i];
+					if (cMap.hasOwnProperty(countryName)) {
+						var countryID = cMap[countryName];
+						var c2 = cMapByID[countryID];
+						usedIDs[countryID] = 1;
+						var mapObject = map.getObjectById(countryID);
+						if (mapObject) {
+							if (activeSphere == "all") {
+								mapObject.color = colors[c2.spheres[0]];
+								mapObject.alpha = 1;
+								mapObject.validate();
+							} else {
+								mapObject.color = colors[activeSphere];
+								mapObject.validate();
+							}
+						}
+					}
+				}
+
+				if (hideUnselectedCountries && activeSphere == 'all') {
+					for (var i=0; i < fullSphereCountryList.length; i++) {
+						var countryID = fullSphereCountryList[i];
+						if (! (usedIDs.hasOwnProperty(countryID))) {
+							var c2 = cMapByID[countryID];
+							var mapObject = map.getObjectById(countryID);
+							if (mapObject) {
+								//console.log('hide ' + countryID);
+								mapObject.color = colors[c2.spheres[0]];
+								mapObject.validate();
+							}
+
+						}
+					}
+				}
+			}
+		});
+
+		map.addListener("rollOverMapObject", function (event) {
+			var countryID = event.mapObject.id;
+			var primarySphere = sMap[countryID];
+
+			if (primarySphere) {
+
+
+				if (activeSphere != "all") {
+					primarySphere = activeSphere;
+				}
+				jQuery('div.dynamicHotSpotText').html('information about ' + primarySphere); 
+				var c = cMapByID[countryID];
+				var s = spheres[primarySphere];
+
+				event.mapObject.color = s.color;
+				event.mapObject.validate();
+
+				//loop through all countries that are in this country's primary sphere
+				var sphereCountries = s.comprisedOf.concat(s.influences);
+				var usedIDs = {};
+				for (var i=0; i < sphereCountries.length; i++) {
+					var countryName = sphereCountries[i];
+					if (cMap.hasOwnProperty(countryName)) {
+						var countryID = cMap[countryName];
+						usedIDs[countryID] = 1;
+						var mapObject = map.getObjectById(countryID);
+						if (mapObject) {
+							if (hideUnselectedCountries) {
+								mapObject.color = s.color;
+							} else {
+								mapObject.color = COLOR_HOVER;
+							}
+							var isComprised = (i < s.comprisedOf.length);
+							var alpha = 1;
+							if ( (primarySphere != "all" && primarySphere != "cve") && !isComprised) {
+								alpha = 0.7;
+								///console.log('comprised!!!!');
+							}
+							mapObject.alpha=alpha;
+
+							mapObject.validate();
+						}
+					}
+				}
+
+				if (hideUnselectedCountries && activeSphere == 'all') {
+					for (var i=0; i < fullSphereCountryList.length; i++) {
+						var countryID = fullSphereCountryList[i];
+						if (! (usedIDs.hasOwnProperty(countryID))) {
+
+							var mapObject = map.getObjectById(countryID);
+							if (mapObject) {
+								// /console.log('hide ' + countryID);
+								mapObject.color = "#ececec";
+								mapObject.validate();
+							}
+
+						}
+					}
+				}
+			}
+		});
+
+	    jQuery('#hotSpotPicker').change(function(e) {
+	    	setActiveSphere(jQuery(this).val());
+	    });
+
+		jQuery('.entity-buttons button').mouseenter(function(e) {
+			resetButtons();
+
+			var val = jQuery(this).val();
+			if (val != activeSphere) {
+				jQuery(this).css('color',colors[val]);
+				jQuery(this).css('background-color','#FFFFFF');
+			}
+		});
+
+		jQuery('.entity-buttons button').mouseleave(function(e) {
+			resetButtons();
+
+			var val = jQuery(this).val();
+			if (val != activeSphere) {
+				jQuery(this).css('color','#FFFFFF');
+				jQuery(this).css('background-color',colors[val]);
+			}
+
+		});
+
+		jQuery('.entity-buttons button').click(function(e) {
+			var val = jQuery(this).val();
+			setActiveSphere(val);
+			resetButtons();
+
+
+			jQuery(this).css('color',colors[val]);
+			jQuery(this).css('background-color',COLOR_ACTIVE);
+
+
+		});
+	    resetButtons();
+	    jQuery('.entity-buttons button.all').trigger( "click" );
+	    initMobileLegend();
+
+	});
+
+})(jQuery,bbgConfig, entities);
