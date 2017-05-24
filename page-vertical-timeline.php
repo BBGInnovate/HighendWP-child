025d532cc75f4ab8d1fddff9fc5aa00697759da0
Joe Flowers <jflowers45@gmail.com>
2017-05-24 14:37:36 -0400

    adding vertical timeline template

---
+++ page-vertical-timeline.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Highend
+ */
+/*
+Template Name: Vertical Timeline
+*/
+?>
+<?php get_header(); ?>
+<?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
+<?php 
+$main_content_style = "";
+if ( vp_metabox('background_settings.hb_content_background_color') )
+	$main_content_style = ' style="background-color: ' . vp_metabox('background_settings.hb_content_background_color') . ';"';
+?> 
+	<!-- BEGIN #main-content -->
+<div id="main-content"<?php echo $main_content_style; ?>>
+	<div class="container">
+		<div class="row main-row">
+			<div id="page-<?php the_ID(); ?>" <?php post_class('col-12'); ?>>
+				
+				<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Open+Sans:400,700' rel='stylesheet' type='text/css'>	<link rel="stylesheet" href="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/style.css">
+	<script src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/modernizr.js"></script><section id="cd-timeline" class="cd-container"><div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-picture.svg" alt="Picture"></div><div class="cd-timeline-content">
+				<h2>Iowa Caucus</h2>
+<div id="images">
+    <a href="https://www.bbg.gov/wp-content/media/2016/02/MBN-IOWA-11.jpg">
+        <img src="https://www.bbg.gov/wp-content/media/2016/02/MBN-IOWA-11.jpg" width="600px" height="120px">
+        <div class="caption">Alhurra TV's Joe Tabet reported the Iowa Caucus from Des Moines, Iowa's state capital.</div>
+    </a>
+
+<p>BBG networks provided around-the-clock coverage of the caucus in dozens of languages. VOA's News Center covered the candidates as they made last-minute appeals for support and issued reports on tight deadline as election officials counted the votes and released final tallies.</p>
+
+<p>VOA language services also provided their own original reporting. The Russian service interviewed Liz Garst, granddaughter of Roswell Garst, who in 1959 welcomed then-Soviet leader Nikita Khruschev to his farm in Coon Rapids, Iowa. Liz Garst now helps organize the Iowa Caucus for the Democratic Party.</p>
+
+<p>Alhurra and Radio Sawa also provided extensive coverage on the role the Iowa Caucus plays in American democracy and what issues most interested the voters when they went to caucus.  The show “Sawa Magazine” examined the impact of the Iowa Caucus on the Democratic and Republican parties.
+
+RFE/RL, for its part, crafted web articles aimed at its audiences in the Balkans, Russia, Ukraine and Iran.</p>
+					<a href="https://www.bbg.gov/2016/02/04/bbg-networks-report-on-iowa-caucus-for-audiences-around-the-world-2/" class="cd-read-more">Read more</a>
+				<span class="cd-date">Feb 1</span>
+			</div>
+		</div><div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-movie.svg" alt="Movie"></div><div class="cd-timeline-content">
+
+				<h2>Super Tuesday</h2>
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fvoiceofamerica%2Fvideos%2F10153543626898074%2F&show_text=0&width=400" width="400" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+				<p>Voice of America's Washington D.C. News Center went live on Facebook for the first time on Super Tuesday to analyze the day's results. </p>
+<p>VOA also produced a piece explaining the Super Tuesday.</p>
+<a href="http://www.voanews.com/a/super-tuesday-democrat-republican-presidential-/3213054.html" class="cd-read-more">Read more</a>
+<span class="cd-date">March 1</span>
+
+			</div></div><div class="cd-timeline-block"><div class="cd-timeline-img cd-picture">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-picture.svg" alt="Picture">
+			</div><div class="cd-timeline-content">
+
+				<h2>Republican National Convention</h2>
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Falhurra%2Fvideos%2F10154217716986136%2F&show_text=0&width=600" width="600" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+
+<p>BBG networks provided live coverage in six languages of Donald Trump's acceptance speech at the Republican National Convention in Cleveland, Ohio.</p>
+<p>Alhurra's simultaneous Arabic translation of the Trump's speech received more than 58,000 views and 3,400 Facebook reactions, comments and shares. Alhurra and Radio Sawa had more than 15 correspondents at the Democratic and the Republican National Conventions each day to report and provide context.</p>
+
+<img src="https://www.bbg.gov/wp-content/media/2016/08/convention-analysis-rnc@3x-1024x805.png" width="600px" height="120px">
+
+				<a href="https://www.bbg.gov/2016/07/22/bbg-networks-carry-trump-speech-live-2/" class="cd-read-more">Read more</a>
+				<span class="cd-date">July 21</span>
+
+			</div></div><div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-location.svg" alt="Location">
+			</div><div class="cd-timeline-content">
+
+				<h2>Democratic National Convention</h2>
+<div id="images">
+    <a href="https://www.bbg.gov/wp-content/media/2016/08/VOA_convention_FBlive-960x624.jpg">
+        <img src="https://www.bbg.gov/wp-content/media/2016/08/VOA_convention_FBlive-960x624.jpg" width="600px" height="120px">
+        <div class="caption">VOA Eurasia Division's Bratislav Djordjevic (L) and Alen Mlatisuma (R) reporting from the DNC in Philadelphia.
+</div>
+    </a>
+				<p>BBG networks provided multi-platform coverage of the Democratic National Convention in Philadelphia.</p>
+<p>VOA Spanish's Facebook livestream of the Clinton’s acceptance speech was viewed by some 150,000 people, while excerpts of other major speeches received more than 200,000 views.</p>
+<p>For many people around the world, the networks served as the only source of accurate and unbiased coverage of the conventions.</p>
+
+<img src="https://www.bbg.gov/wp-content/media/2016/08/convention-analysis-dnc@3x-1024x805.png" width="600px" height="120px">
+
+				<a href="https://www.bbg.gov/2016/08/06/bbg-networks-provide-comprehensive-coverage-u-s-presidential-conventions/" class="cd-read-more">Read more</a>
+				<span class="cd-date">July 29</span>
+
+			</div></div>
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-location.svg" alt="Location">
+			</div><div class="cd-timeline-content">
+
+				<h2>Presidential debates</h2>
+<div id="images">
+    <a href="https://gdb.voanews.com/B195CE1E-3839-404C-8348-C614887A1E45_w650_r1_s.jpg">
+        <img src="https://gdb.voanews.com/B195CE1E-3839-404C-8348-C614887A1E45_w650_r1_s.jpg" width="600px" height="120px">
+        <div class="caption">American voters from various diaspora communities share their thoughts on the final presidential debate, with host Angelica Herrera, October 19, 2016.
+</div>
+    </a>
+				<p>17 immigrant American voters from at least a dozen different diaspora communities participated in post-presidential-debate televised discussion panels at the Voice of America studios.</p>
+<p>Although the panel originated in English, the shows were also translated and simulcast in four other languages: Spanish, Russian, Mandarin and Farsi. Tens of thousands of people watched via Facebook Live and livestream on voanews.com.</p>
+<p><a href="http://learningenglish.voanews.com/a/clinton-and-trump-meet-for-first-presidential-debate/3526939.html" style="color:#0645AD;">VOA learning English created a glossary of key terms</a> mentioned in the debates. RFE/RL summarized <a href="https://www.rferl.org/a/debate-trump-clinton-in-their-own-words/28015743.html" style="color:#0645AD;">Hillary Clinton and Donald Trump's stance on foreign policy issues such as NATO and ISIS.</a><p>
+				<a href="http://www.insidevoa.com/a/the-world-was-watching-voas-post-debate-diaspora-panels/3561070.html" class="cd-read-more">Read more</a>
+				<span class="cd-date">Oct 20</span>
+
+			</div>
+</div>
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-location.svg" alt="Location">
+			</div><div class="cd-timeline-content">
+
+				<h2>Leading up to the Election Day</h2>
+<div id="images">
+    <a href="https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/s960x960/14918971_1832114907003542_6772944340170377402_o.jpg?oh=b979e70d28e982b2433f34c769a511c0&oe=59ABF859">
+        <img src="https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/s960x960/14918971_1832114907003542_6772944340170377402_o.jpg?oh=b979e70d28e982b2433f34c769a511c0&oe=59ABF859" width="600px" height="120px">
+        <div class="caption">The Current Time digital team compared the foreign policy positions of Clinton and Trump.
+</div>
+    </a>
+<p>BBG networks produced extensive campaign coverage, allowing audiences to understand the intricacies of the U.S. election process and the backgrounds of the two candidates.</p> 
+<p>The Current Time digital team issued reports that showed how footage of ballot stuffing in Russian elections was used in a <a href="https://www.facebook.com/currenttimetv/posts/1831734173708282" style="color:#0645AD;">viral Facebook post to claim ballot stuffing in the U.S.</a> and compared how vote rigging allegations are addressed <a href="https://www.currenttime.tv/a/28095431.html" style="color:#0645AD;">by media in the U.S. and in Russia</a>.
+				<p>RFE/RL’s Pangea Digital unit produced an election countdown timer for use by Alhurra on its website, and created an<a href="https://www.currenttime.tv/a/28093235.html" style="color:#0645AD;"> interactive electoral map</a>, localized for 17 RFE/RL language services as well as Arabic for Alhurra.</p>
+<p>RFE/RL Balkan Service produced reports that many of the websites supporting Trump were <a href="https://www.rferl.org/a/macedonia-trump-election-websites/28097576.html" style="color:#0645AD;">created and administered in the Macedonian town of Veles</a>, while RFE/RL's Kyrgyz Service asked <a href="https://rus.azattyk.org/a/28101480.html" style="color:#0645AD;">residents of Bishkek</a> who they thought would win the election. RFE/RL's Uzbek Service asked <a href="http://www.ozodlik.mobi/a/amerika-saylovlari-ozbeklar-hillari-klinton/28103962.html" style="color:#0645AD;">Uzbek Americans who they planned to vote for.</a></p>
+				<span class="cd-date">Nov 7</span>
+
+			</div>
+		</div>
+		</div><div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-movie.svg" alt="Movie"></div><div class="cd-timeline-content">
+
+				<h2>Election Day</h2>
+<div id="images">
+    <a href="https://www.bbg.gov/wp-content/media/2016/11/Alhurra_election-coverage_Dearborn-Michigan-1040x624.jpg">
+        <img src="https://www.bbg.gov/wp-content/media/2016/11/Alhurra_election-coverage_Dearborn-Michigan-1040x624.jpg" width="600px" height="120px">
+        <div class="caption">Alhurra correspondent in Dearborn, Michigan.
+</div>
+    </a>				
+<p>Audiences around the world, including in some of the most press-restrictive countries, had access to full, comprehensive coverage of the historic 58th U.S. presidential election, thanks to the networks of the BBG.</p>
+<p>The VOA — RFE/RL Current Time digital network offered its audience <a href="https://www.youtube.com/watch?v=GSpC0TZV13c" style="color:#0645AD;">eight hours of live Russian-language coverage</a> on election night.</p>
+<p>Several of VOA’s African services connected diaspora communities online during for election night parties. VOA Swahili, for example, went live on Facebook as members of the diaspora from East Africa watched the results come in from a Kenyan restaurant in Beltsville, Maryland.</p>
+<p>Middle East Broadcasting Network's Alhurra Television stationed <a href="https://www.facebook.com/alhurra/videos/10154534735971136/" style="color:#0645AD;">its correspondents throughout the Middle East and North Africa</a>, featuring reactions from Afghanistan, Iraq, Lebanon, Turkey, Iran and elsewhere. The network’s flagship discussion program <a href="https://www.youtube.com/watch?v=pqRCo9p-Eks&feature=youtu.be" style="color:#0645AD;">Free Hour</a> was expanded an additional hour to feature discussion from Republican and Democratic analysts and foreign policy experts from Iraq, Saudi Arabia, Egypt and Iran.</p>
+<p>RFE/RL's Radio Farda provided its radio and Internet audiences with <a href="https://www.radiofarda.com/a/f4_us_election_start_vote_new_president_clinton_trump/28103858.html" style="color:#0645AD;">live updates</a> of the results, as well as a series of features, such as the history of Iranian-American relations with Democratic and Republican administrations in the U.S.</p>
+<p>Radio Azadi, broadcasting in both Dari and Pashto to Afghanistan, liveblogged on election night and conducted interviews with numerous prominent Afghan politicians and residents about expectations for the vote. Azadi also aired a discussion that included <a href="https://www.facebook.com/AzadiR/videos/1185185491574776/" style="color:#0645AD;">Afghanistan President Ashraf Ghani’s press advisor</a>, Nabi Misdaq.</p>
+<p>The RFE/RL newsroom also produced <a href="https://www.rferl.org/a/how-does-the-us-electoral-college-work/28090772.html" style="color:#0645AD;">a video explainer</a>, both in English and Russian, on the U.S. Electoral College, as well as a glossary of key terms and phrases.</p>
+<p>TV and Radio Martí aired more than 20 hours of live continuous coverage and 24-hour live blogging through social media. As part of their coverage, Radio and TV Martí interviewed Cuban immigrants who were voting for the first time, as well as, <a href="https://www.martinoticias.com/a/apoyo-a-trump-divide-voto-cubanoamericano/132630.html" style="color:#0645AD;">other Cuban voters across the country.</a> The Martís also interviewed <a href="https://www.martinoticias.com/a/por-quien-se-inclinan-cubanos-isla-hillary-trump/133208.html" style="color:#0645AD;">Cubans in the island </a>for their thoughts on the election.</p>
+<p>RFA’s Mandarin service covered the election with dispatches from Hong Kong. Even though the Chinese authorities barred live coverage of the vote-counting process in the U.S., the election attracted widespread attention in China.</p>
+<p>VOA Korean <a href="http://www.voanews.com/a/heated-us-election-north-korean-refugees-taste-democracy/3569011.html" style="color:#0645AD;">shared a story </a>with RFAs’ Korean service on a North Korean refugee, resettled in Virginia, as he voted for the first time.</p>
+<a href="https://www.bbg.gov/2016/11/21/bbg-networks-provide-front-row-seat-historic-u-s-elections/" class="cd-read-more">Read more</a>
+				<span class="cd-date">Nov 8</span>
+			</div>
+		</div>
+</div>
+
+<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/cd-icon-movie.svg" alt="Movie"></div><div class="cd-timeline-content">
+
+				<h2>The election's aftermath</h2>
+<iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fvoastudentu%2Fvideos%2F1326931897326017%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+<p>BBG networks provided extensive coverage of reactions to the Trump's victory.</p> 
+<p>VOA’s <a href="http://blogs.voanews.com/student-union/" style="color:#0645AD;">Student Union</a>, a blog geared to international students, spent six hours on election night with students from Washington, D.C. universities, live-streaming their views on the Student Union site, Facebook, Instagram and Twitter.</p>
+<p>Alhurra produced a <a href="https://www.facebook.com/alhurra/videos/vb.307323366135/10154536840041136" style="color:#0645AD;">profile video of Trump</a>, while VOA Persian featured a discussion with the Trump’s Middle East advisor Majid Rafizadeh. RFE/RL’s Radio Farda aired 12.5 hours of election day coverage, and devoted its one-hour morning video program “<a href="https://www.radiofarda.com/z/20371" style="color:#0645AD;">Breakfast with News</a>” on November 9 to analysis of the election results.</p>
+<p>Following the victory of Donald Trump, the Current Time digital team put up a Facebook <a href="https://www.facebook.com/currenttimetv/videos/1832421843639515/" style="color:#0645AD;">live stream of Donald Trump's victory speech</a>, which was viewed 111,000 times and accrued some 20,000 reactions, comments and shares. The team also produced a <a href="https://www.facebook.com/currenttimetv/videos/1832461680302198" style="color:#0645AD;">video compilation of Trump's election promises</a>, and <a href="https://vk.com/currenttime?w=wall-78242229_37096" style="color:#0645AD;">an infographic</a> outlining some of Trump's statements on Russia.</p>
+<p>VOA News Center correspondents in Beijing and Seoul teamed up to produce analysis on how a Trump presidency would affect East Asia, while Radio Free Asia <a href="http://www.rfa.org/english/news/cambodia/cambodia-and-myanmar-11092016141456.html" style="color:#0645AD;">reported on reaction to the vote</a> from China, Myanmar and Cambodia.</p>
+<a href="https://www.bbg.gov/2016/11/21/bbg-networks-engage-audiences-social-media-u-s-elections/" class="cd-read-more">Read more</a>
+				<span class="cd-date">Nov 9</span>
+
+			</div>
+		</div>
+
+
+	</section>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="http://www.bbgannual.com/wp-content/themes/HighendWP-child/timeline/main.js"></script> <!-- Resource jQuery -->
+
+
+				<?php the_content(); ?>
+			</div>
+		</div>
+		<!-- END .row -->
+	</div>
+	<!-- END .container -->
+</div>
+<!-- END #main-content -->
+
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
