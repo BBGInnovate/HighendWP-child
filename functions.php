16cc79c91f2a9d281eb155826281a29c725b282f
Joe Flowers <jflowers45@gmail.com>
2017-05-30 18:23:42 -0400

    app id

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ add_action( 'wp_enqueue_scripts', 'highend_child_theme_enqueue_styles' );
 function fb_appID() {
 ?><meta property="fb:app_id" content="1288914594517692" /><?php
 }
-add_action('wp_head', 'fb_opengraph', 5);
+add_action('wp_head', 'fb_appID', 5);
 
 include_once("functions_maps.php");
 
