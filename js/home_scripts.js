ef5e81a9b3bdf37967b5d3dc14fb9acf82274f9f
Joe Flowers <jflowers45@gmail.com>
2017-06-06 11:08:55 -0400

    moving SVG hover code to global scripts as network svgs is no longer on homepage

--- js/home_scripts.js
+++ js/home_scripts.js
@@ -1,33 +1,11 @@
-function toggleCircleDiv(entity, direction) {
-	if (direction == "out") {
-		jQuery('#circleTooltip').hide();
-	} else {
-		jQuery("div[id^='circleTooltip_']").hide();
-		jQuery('#circleTooltip_'+entity).show();	
-		jQuery('#circleTooltip').show();
-	}
-}
-
 jQuery( document ).ready(function() {
-	jQuery("path[class$='_circle'],g[class$='_circle']").css('cursor','pointer'); 			
-	jQuery("path[class$='_circle'],g[class$='_circle']").hover( 
-		function(e) {
-			var entity = jQuery(this).attr('class').split("_").shift();
-			toggleCircleDiv(entity,'in');
-		},
-		function(e) {
-			toggleCircleDiv('','out');
-		}
-	);
-	jQuery('#circleTooltip').hide() //.addClass('hb-testimonial');
-
 	//there is an issue with the Visual Composer line chart that happens on the hompage
 	//adding this window resize call makes sure that the Waypoint will get recalculated at the appropriate times
 	//so that the animation will be triggered
 	function fakeWindowResize(){
 		jQuery(window).resize();
 	}
-	setInterval(fakeWindowResize, 1500); 
+	setInterval(fakeWindowResize, 500); 
 
 	function showButton() {
 		jQuery('#btnPlay').addClass('importantVisible');
