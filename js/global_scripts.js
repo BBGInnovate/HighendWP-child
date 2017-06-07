b8c407007b07baa915edb6a54f22ba5536fdef96
Joe Flowers <jflowers45@gmail.com>
2017-06-07 15:37:21 -0400

    make board members clickable

--- js/global_scripts.js
+++ js/global_scripts.js
@@ -9,8 +9,11 @@ function toggleCircleDiv(entity, direction) {
 }
 
 jQuery( document ).ready(function() {
+
+	// enable the tooltip that shows up when you highlight text with a maouse
     jQuery('p,h4,h1,h2,h3').selectionSharer();
 
+    // add interactivity to the circular svg on the networks page
     jQuery("path[class$='_circle'],g[class$='_circle']").css('cursor','pointer'); 			
 	jQuery("path[class$='_circle'],g[class$='_circle']").hover( 
 		function(e) {
@@ -22,8 +25,17 @@ jQuery( document ).ready(function() {
 			toggleCircleDiv('','out');
 		}
 	);
-	jQuery('#circleTooltip').hide() //.addClass('hb-testimonial');
+	jQuery('#circleTooltip').hide() 
+
+	//hide the 'Awards' item in the list of category filters, since there is already an 'all' button
 	jQuery("ul.filter-tabs li a[data-filter='.awards']").hide();
+
+	//by default, the images in the list of board members don't link. Let's make them clickable.
+	jQuery('div.team-member-description a.simple-read-more').each(function() {
+		link = jQuery(this).attr('href');
+		jQuery(this).parent().parent().find('img').wrap("<a href='" + link + "'></a>");
+	});
+
 });
 
 
