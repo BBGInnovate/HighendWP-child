b08e01de5f7409b6165168f6dec195c3efea1b71
Gigi Frias <gigi.frias@gmail.com>
2017-06-26 14:28:54 -0400

    Updating "read more" links on the map

--- js/map-hotspot.js
+++ js/map-hotspot.js
@@ -193,7 +193,7 @@ function setActiveSphere(s) {
 	var newAboutText= '';
 	if (s != 'all') {
 		newAboutText = '<h2>' + spheres[s].label + '</h2>' + spheres[s].about;
-		newAboutText += "<BR><BR><span class='readMore'><a href='" + spheres[s].link + "'> Read More »</a></span>";
+		newAboutText += "<br/><br/><span class='more-link'><a href='" + spheres[s].link + "'> Read More</a></span>";
 	}
 	jQuery('div.dynamicHotSpotText').html(newAboutText).selectionSharer();
 	var newAreas = getAreas(s);
